54782616df9127673ccf2d239bb0c277a0823f27
Faiz Arfi <faizarfi12@gmail.com>
2025-03-22 21:59:56 +0530

    Implement change theme button

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,49 @@
+console.log('Script Loaded');
+
+// Initialize theme syste
+initThemeSystem();
+
+function initThemeSystem() {
+    const htmlElement = document.querySelector('html');
+    let currentTheme = getTheme();
+
+    // Initial theme application
+    applyTheme(currentTheme, htmlElement);
+
+    // Add event listener to toggle theme
+    const themeButton = document.querySelector('#theme_change_button');
+    if(themeButton){
+        themeButton.addEventListener('click', function(){
+            currentTheme = currentTheme === 'light'
+? 'dark': 'light';
+            applyTheme(currentTheme, htmlElement);
+        });
+    }else{
+        console.warn('Theme change button not found');
+    }
+}
+
+function getTheme() {
+    try {
+        let theme = localStorage.getItem('theme');
+        return theme ? theme : 'light'; // Default theme is light
+    } catch (error) {
+        console.error('Error getting theme from local storage', error);
+        return 'light'; // Default theme is light
+    }
+}
+
+function applyTheme(theme, element) {
+    element.classList.remove('light', 'dark');
+    element.classList.add(theme);
+    document.querySelector('#theme_text').innerText = theme === 'light' ? 'Dark': 'Light';
+    setTheme(theme);
+}
+
+function setTheme(theme) {
+    try {
+        localStorage.setItem('theme', theme);
+    } catch (error) {
+        console.error('Could not save theme preference', error);
+    }
+}
